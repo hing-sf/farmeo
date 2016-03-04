9d0e7f28f51e6b77f8b3d9bba7f0f9503d4c88e7
Hing Tang <hing.tang.sf@gmail.com>
2016-03-04 11:14:28 -0800

    store file object and pass to server

--- client/app/file-upload/file-upload.controller.js
+++ client/app/file-upload/file-upload.controller.js
@@ -1,45 +1,14 @@
 'use strict';
 
  angular.module('pharmeoApp')
-   .controller('FileUploadCtrl', function ($scope, multipartForm) {
+   .controller('FileUploadCtrl', function ($scope, multipartForm, $rootScope) {
+    var currentUser = $rootScope.currentUser._id;
 
-      // YOU TUBE
-   	$scope.customer = {};
    	$scope.Submit = function(){
-   		var uploadUrl = '/api/upload';
-   		multipartForm.post(uploadUrl, $scope.customer);
+      var file = $scope.file;
+      file.currentUser = currentUser;
+      console.log(file);
+   		var uploadUrl = '/api/pharmacys';
+   		multipartForm.post(uploadUrl, file);
  	}
  });
-
-  // angular.module('pharmeoApp')
-  //   .controller('FileUploadCtrl', function ($scope) {
-
-  //     var getFiles = function(){
-
-  //     }
-
-
-  // })
-  // .config(['flowFactoryProvider', function (flowFactoryProvider) {
-  //   flowFactoryProvider.defaults = {
-  //     target: '/api/upload',
-  //     permanentErrors: [404, 500, 501],
-  //     maxChunkRetries: 1,
-  //     chunkRetryInterval: 5000,
-  //     simultaneousUploads: 4,
-  //     testChunks:false
-  //   };
-  //   flowFactoryProvider.on('catchAll', function (event) {
-  //     console.log('catchAll', arguments);
-  //   });
-  //   //Can be used with different implementations of Flow.js
-  //   //flowFactoryProvider.factory = fustyFlowFactory;
-  // }]);
-
-
-  // app.use(multer({
-  //   dest: './uploads/',
-  //   rename: function (fieldname, filename) {
-  //     return filename.replace(/\W+/g, '-').toLowerCase() + Date.now()
-  //   }
-  // }))
