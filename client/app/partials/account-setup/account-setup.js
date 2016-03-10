3d3ac9b1660fbf01daf3d7c23b93cbde5ca00480
Hing Tang <hing.tang.sf@gmail.com>
2016-03-10 03:38:37 -0800

    pharmacy setup pages, reset password page, pharmacy portal, create icons

--- client/app/partials/account-setup/account-setup.js
+++ client/app/partials/account-setup/account-setup.js
@@ -12,5 +12,15 @@ angular.module('pharmeoApp')
         url: '/account-setup-2',
         templateUrl: 'app/partials/account-setup/account-setup-2.html',
         controller: 'AccountSetupCtrl as vm'
+      })
+      .state('account-setup-3', {
+        url: '/account-setup-3',
+        templateUrl: 'app/partials/account-setup/account-setup-3.html',
+        controller: 'AccountSetupCtrl as vm'
+      })
+      .state('setup-finish', {
+        url: '/setup-finish',
+        templateUrl: 'app/partials/account-setup/setup-finish.html',
+        controller: 'AccountSetupCtrl as vm'
       });
   });
--- client/app/partials/reset-password/reset-password.js
+++ client/app/partials/reset-password/reset-password.js
@@ -7,5 +7,10 @@ angular.module('pharmeoApp')
         url: '/reset-password',
         templateUrl: 'app/partials/reset-password/reset-password.html',
         controller: 'ResetPasswordCtrl'
+      })
+      .state('sent-password-reset', {
+        url: '/sent-password-reset',
+        templateUrl: 'app/partials/reset-password/sent-password-reset.html',
+        controller: 'ResetPasswordCtrl'
       });
   });
